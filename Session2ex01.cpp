7f38063e5e434891584a9efa9dedb511805a7a56
Bangtu21 <bangtu2005.com@gmail.com>
2024-11-20 18:45:02 +0700

    Add files via upload

---
+++ Session2ex01.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main (){
+	int age = 25;//Khai bao va khoi tao bien age kieu int dat gia tri bang 25
+	float weight = 55.5;//Khai bao va khoi tao bien weight kieu float dat gia tri la 55.5
+	char grande = 'A';//Khai bao va khoi tao bien grande kieu char dat gia tri la A
+	double pi = 3.1415;//Khai bao va khoi tao bien pi kieu double dat gia tri laf 3.1415
+	return 0;
+}
