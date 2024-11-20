eb6e5468fc48a3e1d72251d67b36b6a2f0f255e1
Bangtu21 <bangtu2005.com@gmail.com>
2024-11-20 18:45:58 +0700

    Add files via upload

---
+++ Session2ex04.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	int a = 5;
+	int p = a * 4;
+	int s = a * a;
+	printf("Hinh vuong co canh la %d co dien tich la %d\n", a, s);
+	printf("Hinh vuong co canh la %d co chu vi la %d\n", a, p);
+	return 0;
+}
