1ccbb8bf28cf042a8c68b59466361ec76a5b2986
Bangtu21 <bangtu2005.com@gmail.com>
2024-11-20 18:46:35 +0700

    Add files via upload

---
+++ Session2ex06.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int r = 5;
+	float pi = 3.14;
+	float c = r * 2 * pi;
+	float s = r * r * pi;
+	printf("Chu vi hinh tron ban kinh %d la %.1f\n", r, c);
+	printf("Dien tich hinh tron ban kinh %d la %.1f\n", r, s);
+	
+	return 0;
+}
